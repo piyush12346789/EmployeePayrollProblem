81befdddc171d99324bc55673332b88bd606ae45
piyush12346789 <piyushgupta024@gmail.com>
2020-11-01 12:30:34 +0530

    [Piyush] Add: Query to create a table employee_payroll.

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,9 +1,6 @@
-create database payroll_services
-use payroll_services
 create table employee_payroll
 (
 Name varchar(25) not null,
 Salary money not null,
 Start date not null
 )
-select * from employee_payroll
